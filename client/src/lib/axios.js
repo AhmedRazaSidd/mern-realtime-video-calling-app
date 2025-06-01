fd03caf71f94b05d84158ee810055a03a18272c7
AhmedRazaSidd <ar5812264@gmail.com>
2025-06-01 08:30:15 -0700

    Added: Vercel.json

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://mern-realtime-video-calling-app-bac.vercel.app/api',
     withCredentials: true, 
 })
